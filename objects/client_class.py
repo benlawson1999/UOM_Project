24ade3f79abde5214c15825c5f93e95282b6e086
benlawson1999 <87769315+benlawson1999@users.noreply.github.com>
2021-07-29 17:20:37 +0100

    Update client_class.py

--- objects/client_class.py
+++ objects/client_class.py
@@ -1,7 +1,8 @@
 class Client:
-    __slots__ = ["client_id", "postcode", "tenure", "income", "age", "churn_chance"]
-    def __init__(self, client_id, **kwags):
+    __slots__ = ["client_id", "postcode",
+                 "tenure", "income", "age", "churn_chance"]
+
+    def __init__(self, client_id: int, **kwargs):
         self.client_id = client_id
-        for key, value in kwags.items():
-            setattr(self,key,value)
-            
+        for key, value in kwargs.items():
+            setattr(self, key, value)

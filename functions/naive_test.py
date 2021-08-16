986b24bd62170695f94bef906e8705295fd21c1e
benlawson1999 <benjamin.lawson@postgrad.manchester.ac.uk>
2021-08-16 16:48:10 +0100

    updated funtions

--- functions/naive_test.py
+++ functions/naive_test.py
@@ -3,52 +3,62 @@ from functions.WMAPE import wmape
 import matplotlib.pyplot as plt
 import numpy as np
 
-demand = {}
-fulfilled_count = 0
-print(len(Orders))
+def generate_results(results_tuple :dict)  :
+    results_per_method = {}
+    sorted_dict = dict(sorted(results_tuple[2].items(), key=lambda item: item[1]))
+    mean_wmape = results_tuple[0]
+    std_dev = np.std(list(results_tuple[2].values()))
+    results_per_method = {"Mean": mean_wmape, "Standard deviation": std_dev}
+    sorted_dict = dict(sorted(results[2].items(), key=lambda item: item[1]))
+    plt.bar(sorted_dict.keys(), sorted_dict.values(), 1, color="red")
+    print(f"Factory Demand: {results_tuple[1]}")
+    print(f"Mean WMAPE: {mean_wmape}")
+    print(f"Factory-wise WMAPE: {results_tuple[2]}")
+    print(f"Percentage of Orders Fulfilled {results_tuple[3]}")
 
-for order in Orders.values():
+def generate_solutions(orders_dict: dict, Type: str):
+    Type = Type.lower()
+    if Type not in ["naive", "max", "min"]:
+        raise ValueError(
+            "Please only select one of naive, max or min as the objective function type"
+        )
+    demand = {}
+    fulfilled_count = 0
+    for order in Orders.values():
+        order.order_list(Recipes)  # combined the recipes into a list of ingredients
+        if Type == "naive":
+            order.optimal_factory_naive(Factories, demand)
+        elif Type == "max":
+            order.optimal_factory_ranking_max(Factories, demand)
+        elif Type == "min":
+            order.optimal_factory_ranking_min(Factories, demand)
+        # choose the factory with the highest quanitity of ingredients in the order
+        fulfilled_count += order.fulfilled  # 1 added if fulfilled, 0 if not
+        for item, value in order.combined.items():
+            if order.factory_id is None:
+                continue
+            if demand.get(order.factory_id) is None:
+                demand[order.factory_id] = {}  # make a new entry
+            if demand[order.factory_id].get(item) is None:
+                demand[order.factory_id][item] = 0
+            demand[order.factory_id][item] += value
 
-    order.order_list(Recipes)  # combined the recipes into a list of ingredients
-    order.optimal_factory_naive(Factories, demand)
-    # choose the factory with the highest quanitity of ingredients in the order
-    fulfilled_count += order.fulfilled  # 1 added if fulfilled, 0 if not
-    for item, value in order.combined.items():
-        if order.factory_id is None:
-            continue
-        if demand.get(order.factory_id) is None:
+    fulfilled_percent = fulfilled_count / len(Orders) * 100
+    results = {}
+    for factory in Factories.values():
+        if demand.get(factory.factory_id) is None:
+            demand[factory.factory_id] = {}
+        for item, value in Factories[
+            factory.factory_id
+        ].factory_inventory.items():  # make a new entry
+            if demand[factory.factory_id].get(item) is None:
+                demand[factory.factory_id][item] = 0
+            results[factory.factory_id] = wmape(
+                demand[factory.factory_id], factory.factory_inventory
+            )
+        mean_wmape = sum(results.values()) / len(results)
+        results_tuple = tuple([mean_wmape,demand, results, fulfilled_percent])
 
-            demand[order.factory_id] = {}  # make a new entry
-        if demand[order.factory_id].get(item) is None:
-
-            demand[order.factory_id][item] = 0
-
-        demand[order.factory_id][item] += value
-
-
-fulfilled_percent = fulfilled_count / len(Orders) * 100
-results = {}
-for factory in Factories.values():
-    if demand.get(factory.factory_id) is None:
-        demand[factory.factory_id] = {}
-    for item, value in Factories[
-        factory.factory_id
-    ].factory_inventory.items():  # make a new entry
-        if demand[factory.factory_id].get(item) is None:
-            demand[factory.factory_id][item] = 0
-    results[factory.factory_id] = wmape(
-        demand[factory.factory_id], factory.factory_inventory
-    )
-
-sorted_dict = dict(sorted(results.items(), key=lambda item: item[1]))
-results_per_method = {}
-mean_wmape = sum(results.values()) / len(results)
-std_dev = np.std(list(results.values()))
-results_per_method["naive"] = {"Mean": mean_wmape, "Standard deviation": std_dev}
-plt.bar(sorted_dict.keys(), sorted_dict.values(), 1, color="red")
-
-
-print(f"Factory Demand: {demand}")
-print(f"Mean WMAPE: {mean_wmape}")
-print(f"Factory-wise WMAPE: {results}")
-print(f"Percentage of Orders Fulfilled {fulfilled_percent}")
+    return results_tuple
+results = generate_solutions(Orders,"naive")
+generate_results(results)
--- functions/neighbour_search.py
+++ functions/neighbour_search.py
@@ -1,67 +1,36 @@
 from scripts.generate_objects import Factories, Clients, Orders, SKUs, Recipes
 from functions.WMAPE import wmape
+from functions.naive_test import generate_solutions, generate_results
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-demand = {}
-fulfilled_count = 0
 
-orders_rank = list(Orders.items())
+def neighbour_search(orders_dict: dict, Type: str, time_limit=60):
 
-
-def solution_gen(Orders: dict):
-    demand = {}
-    fulfilled_count = 0
-    for order in Orders.values():
-        order.order_list(Recipes)  # combined the recipes into a list of ingredients
-        order.optimal_factory_naive(Factories, demand)
-        # choose the factory with the highest quanitity of ingredients in the order
-        fulfilled_count += order.fulfilled  # 1 added if fulfilled, 0 if not
-        for item, value in order.combined.items():
-            if order.factory_id is None:
-                continue
-            if demand.get(order.factory_id) is None:
-                demand[order.factory_id] = {}  # make a new entry
-            if demand[order.factory_id].get(item) is None:
-                demand[order.factory_id][item] = 0
-            demand[order.factory_id][item] += value
-
-    fulfilled_percent = fulfilled_count / len(Orders) * 100
-    results = {}
-    for factory in Factories.values():
-        if demand.get(factory.factory_id) is None:
-            demand[factory.factory_id] = {}
-        for item, value in Factories[
-            factory.factory_id
-        ].factory_inventory.items():  # make a new entry
-            if demand[factory.factory_id].get(item) is None:
-                demand[factory.factory_id][item] = 0
-            results[factory.factory_id] = wmape(
-                demand[factory.factory_id], factory.factory_inventory
-            )
-    sorted_dict = dict(sorted(results.items(), key=lambda item: item[1]))
-    mean_wmape = sum(results.values()) / len(results)
-    return mean_wmape, results, fulfilled_percent
-
-
-def neighbour_search(Orders: dict, time_limit=120):
+    Type = Type.lower()
+    if Type not in ["naive", "max", "min"]:
+        raise ValueError(
+            "Please only select one of naive, max or min as the objective function type"
+        )
+    orders_rank = list(Orders.items())
     timeout_start = time.time()
     i = 0  # iteration count
     best = (10000,)
-    best_order = list(Orders.keys())
+    best_order = list(orders_dict.keys())
     while time.time() < timeout_start + time_limit:
-        time.sleep(0.25)
+        print(i)
+        time.sleep(0.25)  # allows rest for the cpu to reduce usage
         if i > 4:
-            return best, best_order
-        n = np.random.randint(0, len(Orders) / 2)
+            return best, best_order # if 4 iterations go by without improvement, return current best
+        n = np.random.randint(0, len(orders_dict) / 2)
         orders_head = orders_rank[:n]
         orders_tail = orders_rank[n:]
         np.random.shuffle(orders_tail)
         orders_head.extend(orders_tail)
         new_orders = dict(orders_head)
 
-        results = solution_gen(new_orders)
+        results = generate_solutions(new_orders, Type)
         if results[0] < best[0]:
             best = results
             best_order = list(new_orders.keys())
@@ -71,5 +40,5 @@ def neighbour_search(Orders: dict, time_limit=120):
     return best, best_order
 
 
-results = neighbour_search(Orders)
-print(results)
+results, order = neighbour_search(Orders, "naive")
+generate_results(results)
